2b8f0d4c3e70fb3dca0e2b8fb584fd1828c440ca
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2024-10-18 13:50:11 +0900

    add:ログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+    　flash[:notice] = "登録が完了しました。"
       redirect_to root_path
     else
+    　flash.now.alert = "登録に失敗しました。もう一度確認をお願いします"
       render :new
     end
   end
