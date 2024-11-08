15d40bf53ef60664249a1989b7308ffe35b13932
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2024-11-08 12:03:01 +0900

    新規登録・ログイン・ログアウト機能のデバッグ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    　flash = "登録が完了しました。"
+      flash[:notice] = "登録が完了しました。"
       redirect_to root_path
     else
-      flash.now.alert = "登録に失敗しました。もう一度確認をお願いします"
+      flash.now[:alert] = "登録に失敗しました。もう一度確認をお願いします"
       render :new
     end
+
   end
 
   private
