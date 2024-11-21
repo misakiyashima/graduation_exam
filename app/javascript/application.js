b7b5339f5591fcfb859efd2bf94de764c85c018e
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2024-11-21 23:55:34 +0900

    add:nodeman・esbuildのDL

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,13 +7,13 @@ import "./controllers"
 import * as bootstrap from "bootstrap"
 
 import { Application } from "@hotwired/stimulus"
-import { Autocomplete } from 'stimulus-autocomplete'  #コンポーネントを読み込むための記述
+import { Autocomplete } from 'stimulus-autocomplete' // コンポーネントを読み込むための記述
 
 const application = Application.start()
-application.register('autocomplete', Autocomplete)  #コンポーネントにあるAutocompleteコントローラを使えるようにするための記述
+application.register('autocomplete', Autocomplete) // コンポーネントにあるAutocompleteコントローラを使えるようにするための記述
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
