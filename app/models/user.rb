56b2acbc837e4485e7fe94646433354bd780f73a
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2024-11-29 00:08:26 +0900

    userモデルへの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, presence: true, uniqueness: true
+
+  has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmark_boards, through: :bookmarks, source: :hotel
   
     def self.login(email, password)
       user = find_by(email: email)
