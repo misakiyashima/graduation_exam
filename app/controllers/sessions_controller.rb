0cfd1b8b3e4e89c5caedbb861607e1a88659ea81
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2024-12-07 13:20:30 +0900

    お気に入り機能修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  protect_from_forgery with: :exception
+  
   def new
   end
 
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,6 @@
 class Bookmark < ApplicationRecord
+  belongs_to :user
+  belongs_to :hotel
+
+  validates :user_id, uniqueness: { scope: :board_id }
 end
