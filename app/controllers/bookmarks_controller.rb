b904395e31cdc2a11a12ac0d092edb71be3e188c
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2024-12-09 16:49:25 +0900

    add:bookmarkコントローラ

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,9 +1,13 @@
 class BookmarksController < ApplicationController
   def create
-
+    hotel = Hotel.find(params[:hotel_id])
+    current_user.bookmark(hotel)
+    redirect_to hotels_path, success:'成功'
   end
 
   def dastroy
-
+  　hotel = current_user.bookmarks.find(params[:id]).hotel
+    current_user.unbookmark(hotel)
+    redirect_to hotels_path, success:'成功', status: :see_other
   end
 end
