4ff3328d4e0da1f98d9c01824a96304496e9a70e
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2024-12-11 15:26:58 +0900

    検索結果viewファイルの修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,12 @@ class BookmarksController < ApplicationController
   def create
     hotel = Hotel.find(params[:hotel_id])
     current_user.bookmark(hotel)
-    redirect_to hotels_path, success:'成功'
+    redirect_to hotels_path, success:'お気に入りに追加しました'
   end
 
-  def dastroy
+  def destroy
   　hotel = current_user.bookmarks.find(params[:id]).hotel
     current_user.unbookmark(hotel)
-    redirect_to hotels_path, success:'成功', status: :see_other
+    redirect_to hotels_path, success:'お気に入りを解除しました', status: :see_other
   end
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,5 +1,6 @@
 class Hotel < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
+  has_many :bookmarked_by_users, through: :bookmarks, source: :user
 
 end
