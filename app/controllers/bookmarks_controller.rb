dea55b58868c5cfc3063d97d63382040774ba2e6
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-01-11 23:04:29 +0900

    add:bookmarksコントローラへブックマークの存在確認

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,18 +8,23 @@ class BookmarksController < ApplicationController
       # ホテル情報をデータベースに保存
       hotel_service.save_hotel_to_db(hotel_details)
       
-      bookmark = current_user.bookmarks.create(
-        hotel_id: params[:hotel_id],
-        hotel_no: hotel_details['hotelNo'],
-        hotel_name: hotel_details['hotelName'],
-        hotel_information_url: hotel_details['hotelInformationUrl']
-      )
-      
-      Rails.logger.info "Bookmark Errors: #{bookmark.errors.full_messages}"
-      if bookmark.persisted?
-        redirect_to hotels_path, success: 'お気に入りに追加しました'
+      # ブックマークの存在確認
+      unless current_user.bookmarks.exists?(hotel_id: params[:hotel_id])
+        bookmark = current_user.bookmarks.create(
+          hotel_id: params[:hotel_id],
+          hotel_no: hotel_details['hotelNo'],
+          hotel_name: hotel_details['hotelName'],
+          hotel_information_url: hotel_details['hotelInformationUrl']
+        )
+        
+        Rails.logger.info "Bookmark Errors: #{bookmark.errors.full_messages}"
+        if bookmark.persisted?
+          redirect_to hotels_path, success: 'お気に入りに追加しました'
+        else
+          redirect_to hotels_path, alert: "お気に入りの追加に失敗しました: #{bookmark.errors.full_messages.join(', ')}"
+        end
       else
-        redirect_to hotels_path, alert: "お気に入りの追加に失敗しました: #{bookmark.errors.full_messages.join(', ')}"
+        redirect_to hotels_path, alert: 'このホテルは既にお気に入りに追加されています'
       end
     else
       redirect_to hotels_path, alert: 'ホテルが見つかりませんでした'
