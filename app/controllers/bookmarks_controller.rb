04a0dfc112813b7874a7159579c1811e27c0d3de
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-01-21 23:19:45 +0900

    各ページ遷移の修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,11 +5,11 @@ class BookmarksController < ApplicationController
     hotel_no = params[:hotel_id]
     hotel_service = HotelService.new(ENV['RAKUTEN_API_KEY'])  # APIキーを渡す
     hotel_details = hotel_service.get_hotel_details(hotel_no)
-    
+  
     if hotel_details.present?
       # ホテル情報をデータベースに保存
       hotel_service.save_hotel_to_db(hotel_details)
-      
+    
       # ブックマークの存在確認
       unless current_user.bookmarks.exists?(hotel_id: params[:hotel_id])
         bookmark = current_user.bookmarks.create(
@@ -18,31 +18,30 @@ class BookmarksController < ApplicationController
           hotel_name: hotel_details['hotelName'],
           hotel_information_url: hotel_details['hotelInformationUrl']
         )
-        
+      
         Rails.logger.info "Bookmark Errors: #{bookmark.errors.full_messages}"
         if bookmark.persisted?
-          redirect_to search_hotels_path(keyword: params[:keyword]), notice: 'お気に入りに追加しました'
+          redirect_to bookmarks_hotels_path, notice: 'お気に入りに追加しました'
         else
-          redirect_to hotels_path, alert: "お気に入りの追加に失敗しました: #{bookmark.errors.full_messages.join(', ')}"
+          redirect_to bookmarks_hotels_path, alert: "お気に入りの追加に失敗しました: #{bookmark.errors.full_messages.join(', ')}"
         end
       else
-        redirect_to hotels_path, alert: 'このホテルは既にお気に入りに追加されています'
+        redirect_to bookmarks_hotels_path, alert: 'このホテルは既にお気に入りに追加されています'
       end
     else
-      redirect_to hotels_path, alert: 'ホテルが見つかりませんでした'
+      redirect_to bookmarks_hotels_path, alert: 'ホテルが見つかりませんでした'
     end
   end
 
-def destroy
-  bookmark = current_user.bookmarks.find_by(id: params[:id])
-  if bookmark
-    bookmark.destroy
-    redirect_to bookmarks_hotels_path, notice: 'お気に入りを解除しました', status: :see_other
-  else
-    redirect_to bookmarks_hotels_path, alert: 'お気に入りが見つかりませんでした', status: :see_other
+  def destroy
+    bookmark = current_user.bookmarks.find_by(id: params[:id])
+    if bookmark
+      bookmark.destroy
+      redirect_to bookmarks_hotels_path, notice: 'お気に入りを解除しました', status: :see_other
+    else
+      redirect_to bookmarks_hotels_path, alert: 'お気に入りが見つかりませんでした', status: :see_other
+    end
   end
-end
-
 
   private
 
