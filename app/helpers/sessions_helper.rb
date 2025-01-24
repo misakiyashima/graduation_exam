9b00609916f5fcd1dc534e8addede4802f515b92
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-01-24 14:51:32 +0900

    add:セッション管理用のヘルパーメソッド

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,27 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:uid] = user.uid
+  end
+
+  # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    @current_user ||= User.find_by(uid: session[:uid]) if session[:uid]
+  end
+
+  # 受け取ったユーザーがログイン中のユーザーと一致すればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:uid)
+    @current_user = nil
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['wRFHOrusnaCk9vfRuuVTwew8B'], ENV['Y5pQopgpzcylCqxmjmamAUSP4dr1fgWuTEbL0KXN0eo53sTK87']
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   OmniAuth.config.on_failure = Proc.new { |env|
-  OmniAuth::FailureEndpoint.new(env).redirect_to_failure ##コールバックに失敗した時のアクション設定
-}
+    OmniAuth::FailureEndpoint.new(env).redirect_to_failure ##コールバックに失敗した時のアクション設定
+  }
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get]
