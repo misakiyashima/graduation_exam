49df0b4629889e2f5b0e23a8007645630eb86458
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-01-31 22:46:34 +0900

    fix:tagsコントローラ

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,18 @@
 class TagsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def create
+    hotel_tag = HotelTag.new(hotel_tag_params)
+    if hotel_tag.save
+      redirect_to root_path, notice: "タグが追加されました"
+    else
+      redirect_to root_path, alert: "タグの追加に失敗しました"
+    end
+  end
+
+  private
+
+  def hotel_tag_params
+    params.require(:hotel_tag).permit(:hotel_id, :tag_id)
+  end
 end
