287de43fda707ef81cd062a212566fea7bf0dfbd
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-02-08 00:57:33 +0900

    fix:tagフォーム画面

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,20 +1,18 @@
 class TagsController < ApplicationController
   before_action :require_login, only: [:new, :create, :destroy]
+  before_action :set_tags, only: [:new, :create]
 
   def new
     @hotel_tag = HotelTag.new
-    @hotels = Hotel.all
-    @tags = Tag.all
+    @hotel_id = params[:hotel_id]
   end
 
   def create
     @hotel_tag = HotelTag.new(hotel_tag_params)
-    puts hotel_tag_params.inspect # デバッグ出力
     if @hotel_tag.save
       redirect_to root_path, notice: "タグが追加されました"
     else
-      @hotels = Hotel.all
-      @tags = Tag.all
+      @hotel_id = @hotel_tag.hotel_id
       render :new, alert: "タグの追加に失敗しました"
     end
   end
@@ -32,5 +30,9 @@ class TagsController < ApplicationController
 
   def hotel_tag_params
     params.require(:hotel_tag).permit(:hotel_id, :tag_id)
+  end
+
+  def set_tags
+    @tags = Tag.all
   end
 end
