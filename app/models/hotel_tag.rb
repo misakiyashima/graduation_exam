fcfe9109526d84d29fe21ac66c601d408476ce66
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-02-07 23:56:03 +0900

    fix:tagフォームの一部

--- app/models/hotel_tag.rb
+++ app/models/hotel_tag.rb
@@ -2,6 +2,8 @@ class HotelTag < ApplicationRecord
   belongs_to :hotel
   belongs_to :tag
 
+  attr_accessor :hotel_id, :tag_id
+
   validates :hotel_id, presence: true
   validates :tag_id, presence: true
 end
