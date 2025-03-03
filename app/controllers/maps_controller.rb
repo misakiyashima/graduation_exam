8d45e70fe80b4bc939603d52ea671d27a6466527
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-03-03 23:47:36 +0900

    fix:googlemap表示

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,2 +1,7 @@
 class MapsController < ApplicationController
+  def index
+    @google_maps_api_key = ENV['GOOGLE_MAPS_API_KEY']
+    hotel_service = HotelService.new(ENV['RAKUTEN_API_KEY'])
+    @hotels = hotel_service.search_all_inclusive_hotels('オールインクルーシブ')
+  end
 end
