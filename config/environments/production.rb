02a10d577a839771b3513448ff198267e1c1111b
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-03-20 19:29:55 +0900

    fix:config/environments/production.rbのDotenv loading 設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,5 +99,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  Dotenv::Railtie.load
+  if Rails.env.development? || Rails.env.test?
+    Dotenv::Railtie.load
+  end
 end
