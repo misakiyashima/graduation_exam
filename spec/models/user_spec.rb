92a344e74ea339a9d97da5622afb0d550bc02699
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-04-11 23:59:44 +0900

    fix:DatabaseCleaner

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,3 +33,18 @@ RSpec.describe User, type: :model do
     end
   end
 end
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
