2aa453efc275a360338591315cfcc3a309e5b077
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-07-08 23:22:03 +0900

    fix:app/javascript/controllers/index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,15 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// app/javascript/controllers/index.js
+import { application } from "./application"
+
+// import.meta.globEager で _controller.js をまとめて読み込み
+const controllers = import.meta.globEager("./**/*_controller.js")
+
+// ファイル名からコントローラ名を生成して登録
+Object.entries(controllers).forEach(([path, mod]) => {
+  // "./hello_controller.js" → "hello"
+  const name = path
+    .split("/")
+    .pop()
+    .replace(/_controller\.js$/, "")
+  application.register(name, mod.default)
+})
