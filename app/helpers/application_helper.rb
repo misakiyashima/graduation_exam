39bab8d695820e4ceb4a362dfd1e58159d638236
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-08-31 23:56:51 +0900

    add:リダイレクト先のホワイトリスト化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # params[:return_to] が同一ホスト内のパスなら返し、そうでなければ fallback を返す
+  def safe_return_to(url, fallback)
+    uri = URI.parse(url.to_s)
+    if uri.host.nil? && uri.path.start_with?("/") && uri.scheme.nil?
+      uri.to_s
+    else
+      fallback
+    end
+  rescue URI::InvalidURIError
+    fallback
+  end
 end
