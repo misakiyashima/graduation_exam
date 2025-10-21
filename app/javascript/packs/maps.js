bfa22cf2272c1029b06b4879984c858e5e138c2b
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-10-22 01:39:01 +0900

    fix:initMapの動的追加コードを削除

--- app/javascript/packs/maps.js
+++ app/javascript/packs/maps.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Google Maps APIのスクリプトをロード
   const script = document.createElement('script');
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=initMap`;
+  //script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=initMap`;
   script.async = true;
   script.defer = true;
   document.body.appendChild(script);
