1c9289401901c26005827c447d4b9ddf87a77a3f
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2025-12-07 23:19:35 +0900

    fix:tagController

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,36 +8,36 @@ class TagsController < ApplicationController
   end
 
   def create
-  hotel_id = hotel_tag_params[:hotel_id]
-  return_to = params[:return_to]
-  tag_name = params[:hotel_tag][:tag_id]
+    hotel_id = hotel_tag_params[:hotel_id]
+    return_to = params[:return_to]
+    tag_name = params[:hotel_tag][:tag_id]
 
   # 楽天APIからホテル情報を取得
-  hotel_service = HotelService.new(ENV["RAKUTEN_API_KEY"])
-  hotel_info = hotel_service.get_hotel_details(hotel_id)
+    hotel_service = HotelService.new(ENV["RAKUTEN_API_KEY"])
+    hotel_info = hotel_service.get_hotel_details(hotel_id)
 
-  if hotel_info.present?
+    if hotel_info.present?
     # ホテル情報を保存
-    hotel_service.save_hotel_to_db(hotel_info)
-  else
-    redirect_to root_path, alert: "無効なホテルIDです"
-    return
-  end
+      hotel_service.save_hotel_to_db(hotel_info)
+    else
+      redirect_to root_path, alert: "無効なホテルIDです"
+      return
+    end
 
-  # タグ作成処理
-  tag = Tag.find_or_create_by(name: tag_name)
-  @hotel_tag = HotelTag.new(hotel_id: hotel_id, tag_id: tag.id)
+   # タグ作成処理
+      tag = Tag.find_or_create_by(name: tag_name)
+      @hotel_tag = HotelTag.new(hotel_id: hotel_id, tag_id: tag.id)
 
-  @hotel_tag.user_id = current_user.id if current_user
+      @hotel_tag.user_id = current_user.id if current_user
 
-  if @hotel_tag.save
-    redirect_to return_to.present? ? return_to : root_path, notice: "タグが追加されました"
+    if @hotel_tag.save
+      redirect_to return_to.present? ? return_to : root_path, notice: "タグが追加されました"
 
-  else
-    @hotel_id = hotel_id
-    set_tags
-    render :new, alert: "タグの追加に失敗しました"
-  end
+    else
+      @hotel_id = hotel_id
+      set_tags
+      render :new, alert: "タグの追加に失敗しました"
+    end
   end
 
   def destroy
