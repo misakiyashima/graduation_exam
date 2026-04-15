c90f42d43395651ece9f0f3542d58ad57f585ea2
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2026-04-16 00:21:20 +0900

    fix:ユーザー作成ロジックはXとgoogle共通でUserモデルに集約

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -25,16 +25,15 @@ class OauthCallbacksController < ApplicationController
 
   def google
     auth = request.env['omniauth.auth']
-    #Googleはemailを返すためemailを保存できる
-    user = User.find_or_create_by(provider: auth.provider, uid: auth.uid) do |u|
-      u.email = auth.info.email
-      u.name  = auth.info.name
-      u.avatar_url = auth.info.image
-      u.password = SecureRandom.hex(10) # Sorcery用
+
+    if auth.nil? || !auth["provider"] || !auth["uid"]
+      redirect_to new_user_path, alert: "認証情報が取得できませんでした。"
+      return
     end
-#Sorceryのauto_loginを使う.Twitterと異なり、例外処理不要
+#ユーザー作成ロジックは共通で User.from_omniauth に集約
+    user = User.from_omniauth(auth)
+#Sorceryのauto_login を使う(レスポンスが安定しているため)
     auto_login(user)
     redirect_to root_path, notice: "Googleでログインしました"
   end
-
 end
